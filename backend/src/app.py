ef0ba751e428b715116686c8512ae7f78ce5bffd
Minh Q Nguyen <116717692+QuangMinhNguyen27405@users.noreply.github.com>
2026-03-31 21:15:42 -0400

    "Add Env Variables" (#117)
    * "add env variables"
    
    * Modify app.py

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,10 +1,20 @@
+import os
+import boto3
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.utilities.typing.lambda_context import LambdaContext
 
-from src.shared.auth import auth_middleware
-from src.shared.exceptions import exception_middleware
+from boto3.dynamodb.conditions import Key
+
+from shared.auth import auth_middleware
+from shared.exceptions import exception_middleware
 
 app = APIGatewayRestResolver()
+
+TABLE_NAME = os.environ.get("TABLE_NAME")
+BUCKET_NAME = os.environ.get("BUCKET_NAME")
+
+dynamodb = boto3.resource("dynamodb")
+s3 = boto3.client("s3")
 
 @app.get("/health")
 def health_check():
